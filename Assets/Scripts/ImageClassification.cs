eb52f06cd995ff7e0c0f136acc37ef960d9c34bd
leochocolat <l.mouraire@outlook.fr>
2020-11-02 15:39:54 +0100

    setup Barracuda

---
+++ Assets/Scripts/ImageClassification.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Barracuda;
+
+public class ImageClassification : MonoBehaviour
+{
+
+    public NNModel modelAsset;
+    public Texture2D texture;
+    private Model m_RuntimeModel;
+    private IWorker m_Worker;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_RuntimeModel = ModelLoader.Load(modelAsset);
+        m_Worker = WorkerFactory.CreateWorker(WorkerFactory.Type.Compute, m_RuntimeModel);
+
+        Tensor input = new Tensor(texture);
+        m_Worker.Execute(input);
+        var output = m_Worker.PeekOutput();
+        input.Dispose();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
